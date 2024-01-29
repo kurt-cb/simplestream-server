e0f92d4e9087713f4ae6d25d5e85c46e56a8870a
Kurt Godwin <kgodwin@itron.com>
2024-01-29 15:56:32 -0500

    wip

--- bottle_test/wsgi.py
+++ bottle_test/wsgi.py
@@ -1,9 +1,14 @@
 from bottle_test.wsgi import Bottle, template
+import bottle
 
 app = application = Bottle()
 
 @app.route('/hello/<name>')
 def hello(name):
     return template('Hello, {{name}}!', name=name)
+
+@bottle.route('/there/<name>')
+def there(name):
+    return template('there, {{name}}!', name=name)
 
 # run(app, host='localhost', port=8080)
---
+++ upload_server/wsgi.py
@@ -0,0 +1,4 @@
+from bottle_test.wsgi import Bottle, template
+import upload_server
+
+app = application = Bottle()
