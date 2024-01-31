d1adc3082ca618410f0e51c7fb012305f77a6c39
Kurt Godwin <kgodwin@itron.com>
2024-01-30 20:40:21 -0500

    mount images

--- createContainerUnit.sh
+++ createContainerUnit.sh
@@ -73,7 +73,9 @@ else
    lxc exec simplestream -- bash -c "cd /opt;mkdir -p lxd-image-server;cd lxd-image-server; tar xf  ../files.tar.bz2 --no-same-owner;chown root:adm -R /opt "
 fi
 # configure the container with nginx and install the python code
-lxc config device add simplestream images disk source=$(pwd)/images path=/var/www/simplestreams/images
+mkdir -p images
+lxc config device add simplestream images disk source=$(pwd)/images path=/mnt/images
+printf "uid $(id -u) 0\ngid $(id -g) 0" | lxc config set simplestream raw.idmap -
 lxc exec simplestream -- bash -c /opt/lxd-image-server/scripts/config_container_unit.sh
 
 cat <<EOF
--- scripts/config_container_unit.sh
+++ scripts/config_container_unit.sh
@@ -32,7 +32,7 @@ apt-get install $OPTIONS \
    gnupg2 apt-transport-https ca-certificates curl wget \
    strace curl wget netcat nano git patch net-tools build-essential \
    cmake libdbus-glib-1-dev libgirepository1.0-dev \
-   libcairo2-dev pkg-config python3-dev
+   libcairo2-dev pkg-config python3-dev dbus-x11
 
 sudo curl --output /usr/share/keyrings/nginx-keyring.gpg  \
       https://unit.nginx.org/keys/nginx-keyring.gpg
@@ -69,6 +69,7 @@ cp -r html /var/www
 mkdir -p /var/www/simplestreams
 chown -R unit:unit /var/www
 chmod -R g+s /var/www/simplestreams
+ln -s /mnt/images /var/www/simplestreams/images
 chmod -R g+w /var/www
 cp scripts/user_config.sh /home/ubuntu
 cd ..
